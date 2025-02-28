599521858c00e9e7d4b1ece23331b431d8da1018
Pressense Fasani <camillefasani@gmail.com>
2025-02-28 13:55:03 +0100

    HORS-TEMPS Etape 5

--- script.js
+++ script.js
@@ -7,11 +7,25 @@ quotationForm.addEventListener('submit', submitAQuote)
 
 function submitAQuote(event){
     event.preventDefault();
-    let text = quoteText.value;
+    let quote = quoteText.value;
     let author = quoteAuthor.value;
-    console.log(text + " " + author);
+    addQuote(quote,author);
 }
 
+function addQuote(quote, author){
+    const textParagraph = document.createElement('p');
+    textParagraph.innerHTML = quote;
+    textParagraph.classList.add("text");
 
+    const authorParagraph = document.createElement('p');
+    authorParagraph.innerHTML = author;
+    authorParagraph.classList.add("author");
 
-    
+    const quoteZone = document.createElement('div');
+    quoteZone.classList.add("quote");
+    quoteZone.appendChild(textParagraph);
+    quoteZone.appendChild(authorParagraph);
+
+    const quoteList = document.querySelector("#quote-list");
+    quoteList.appendChild(quoteZone);
+}
